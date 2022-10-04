1704ac9d5634b01f59d769940e619e74c7086177
Colby Todd <ctodd083@uottawa.ca>
2022-10-04 14:37:01 -0400

    Updated design 2

--- pointcp/design3/PointCP.java
+++ pointcp/design3/PointCP.java
@@ -86,37 +86,7 @@ public class PointCP
   }
   
 	
-  /**
-   * Converts Cartesian coordinates to Polar coordinates.
-   */
-  public void convertStorageToPolar()
-  {
-    if(typeCoord != 'P')
-    {
-      //Calculate RHO and THETA
-      double temp = getRho();
-      yOrTheta = getTheta();
-      xOrRho = temp;
-      
-      typeCoord = 'P';  //Change coord type identifier
-    }
-  }
-	
-  /**
-   * Converts Polar coordinates to Cartesian coordinates.
-   */
-  public void convertStorageToCartesian()
-  {
-    if(typeCoord != 'C')
-    {
-      //Calculate X and Y
-      double temp = getX();
-      yOrTheta = getY();
-      xOrRho = temp;
-   
-      typeCoord = 'C';	//Change coord type identifier
-    }
-  }
+
 
   /**
    * Calculates the distance in between two points using the Pythagorean
